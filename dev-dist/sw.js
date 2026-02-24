e03799dc86778a3d7a8c99364315caad4c3b87ea
MaryGomez19 <2123200430@soy.utj.edu.mx>
2026-02-24 17:34:40 -0600

    Agregue linea 4 de api.ts

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3i0l2ahrtio"
+    "revision": "0.tsp7lruereg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5555/api',
+    withCredentials: true,
 });
 
 export function setAuth(token: string | null) {
