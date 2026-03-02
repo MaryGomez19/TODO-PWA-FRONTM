ce2847abdd3cfed17aeb5028df412b5a0e9d452d
MaryGomez19 <2123200430@soy.utj.edu.mx>
2026-03-01 19:41:56 -0600

    Todo correcto hasta este momento

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kbkfj8u1d5"
+    "revision": "0.igd0u3nnkuo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
